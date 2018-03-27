3ca0bcbee92b808a1089ccc8cf1592734b33830f
Drew Clements <drewclementsdesign@gmail.com>
2018-03-27 19:22:51 -0400

    Added mobile responsive navigation

--- javascript/local.js
+++ javascript/local.js
@@ -1,6 +1,19 @@
+//fade in on load
 const body = document.getElementById("body");
 
 window.onload = function() {
     body.classList.add("fadeIn");
 };
+
+
+//toggle nav
+function toggleNav() {
+    const element = document.getElementById("navToggle")
+    
+    element.classList.toggle("navCollapse");
+}
+
+
+
+
 
