08c51b42ea4c80e6960213c9a2377bd64651bcdd
Drew Clements <drewclementsdesign@gmail.com>
2019-05-01 00:30:18 -0400

    Initial Commit
    Set up page bases
    Navigation styling

---
+++ src/pages/blog.js
@@ -0,0 +1,15 @@
+import React from "react"
+//import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Blog = () => (
+  <Layout>
+    <SEO title="Blog" />
+    <h1>Blog</h1>
+    <p>Welcome to Blog</p>
+  </Layout>
+)
+
+export default Blog
