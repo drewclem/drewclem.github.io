264acdbe52e83c5524ba9205f8494ea416c97e56
Drew Clements <drewclementsdesign@gmail.com>
2019-08-19 22:04:29 -0400

    Updated workplace on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => (
         <h1>Develop<br />er <span>&amp;</span> Design<br />er</h1>
         <h3>Drew Clements</h3>
         <div className='sidebar'></div>
-        <p>Hi! I'm just a dad trying to do right for his wife and daughter! I work as a developer with the amazing team at <a href='https://www.thirdwavedigital.com'  target='_blank' rel='noopener noreferrer' style={{color: "var(--red)"}}>Third Wave Digital</a>. I spend my spare time exploring the JAMstack world and building <a href='https://coltxp.com' style={{color: "var(--red)"}} target='_blank' rel='noopener noreferrer'>ColtXP</a>.</p>
+        <p>Hi! I'm just a dad trying to do right for his wife and daughter! I work as a designer at <a href='https://365degreetotalmarketing.com/'  target='_blank' rel='noopener noreferrer' style={{color: "var(--red)"}}>365 Total Marketing</a> and as a junior developer with <a href='https://www.designkollective.com' target='blank' rel='noopener noreferrer' style={{color: "var(--red)"}}>Design Kollective</a>. I spend my spare time exploring the JAMstack world and building <a href='https://coltxp.com' style={{color: "var(--red)"}} target='_blank' rel='noopener noreferrer'>ColtXP</a>.</p>
       </div>
 
       <div className='heroCTA'>
