69a3005402ed9e0b33a97eaa661ab7ae5e9e946d
Drew Clements <36208622+drewclem@users.noreply.github.com>
2020-05-10 22:01:56 -0400

    Updating projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,27 +1,82 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { faGithub, faCodepen } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faCodepen } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Projects = () => (
   <Layout>
-
     <SEO title="Projects" />
-    <div className='interior-anchor'></div>
+    <div className="interior-anchor" />
 
     <h1 className="page-title">Projects</h1>
+    <div className="project-item">
+      <h2>Protege.dev</h2>
 
-    <div className='project-item'>
+      <div className="project-copy">
+        <p>
+          Protege.dev is a job board tailored for junior developers looking for
+          remote opportunities. This project has end-to-end testing with
+          Crypress.io, form validation with Formik and Yup, and wired up to a
+          Firebase database for the back-end.
+        </p>
+
+        <h3
+          style={{
+            marginBottom: 10,
+            fontSize: 18,
+          }}
+        >
+          Tech:
+        </h3>
+
+        <ul>
+          <li>React</li>
+          <li>Firebase</li>
+          <li>Tailwind CSS</li>
+          <li>Cypress.io</li>
+        </ul>
+
+        <a
+          href="https://protege.dev"
+          className="btn-red"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View
+        </a>
+
+        <a
+          href="https://github.com/drewclem/protege"
+          className="github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon className="github" icon={faGithub} />
+        </a>
+      </div>
+    </div>
+
+    <div className="project-item">
       <h2>ColtXP</h2>
 
-      <div className='project-copy'>
-        <p>Colt is a place where beginners looking to gain real-world experience can pair with seasoned developers to work through github issues on live projects. Everyone starts at the bottom, but not everyone has someone to help them along the way. ColtXP's aim is to change that by giving juniors a place to find senior dev's willing to pay it forward.</p>
+      <div className="project-copy">
+        <p>
+          Colt is a place where beginners looking to gain real-world experience
+          can pair with seasoned developers to work through github issues on
+          live projects. Everyone starts at the bottom, but not everyone has
+          someone to help them along the way. ColtXP's aim is to change that by
+          giving juniors a place to find senior dev's willing to pay it forward.
+        </p>
 
-        <h3 style={{
-          marginBottom: 10,
-          fontSize: 18
-        }}>Tech:</h3>
+        <h3
+          style={{
+            marginBottom: 10,
+            fontSize: 18,
+          }}
+        >
+          Tech:
+        </h3>
 
         <ul>
           <li>Vue/Nuxt</li>
@@ -29,60 +84,124 @@ const Projects = () => (
           <li>Tailwind CSS</li>
         </ul>
 
-        <a href='https://coltxp.com' className='btn-red' target='_blank' rel='noopener noreferrer'>View</a>
+        <a
+          href="https://coltxp.com"
+          className="btn-red"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View
+        </a>
 
-        <a href='https://github.com/drewclem/Colt' className='github' target='_blank' rel='noopener noreferrer'>
-          <FontAwesomeIcon className='github' icon={faGithub}/>
+        <a
+          href="https://github.com/drewclem/Colt"
+          className="github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon className="github" icon={faGithub} />
         </a>
       </div>
     </div>
 
-    <div className='project-item'>
-      <h2>TiftonHog</h2>
+    <div className="project-item">
+      <h2>FNF APP</h2>
 
-      <div className='project-copy'>
-        <p>Tiftonhoggroup.com is a small members site for a Harley's Owner Group in South Georgia. The challenge with this design was to take the limited amount of content and present it in a pleasing way that didn't leave the page feeling empty. The solution was to highlight the aspects they enjoy the most - recent rides, upcoming events, and the bond they share with the other members.</p>
+      <div className="project-copy">
+        <p>
+          The FNF app is a full-stack project with a Node API hosted on heroku
+          and a Gatsby front-end hosted on Netlify.
+        </p>
 
-        <h3 style={{
-          marginBottom: 10,
-          fontSize: 18
-        }}>Tech:</h3>
-
-        <ul>
-          <li>Coldfusion</li>
-          <li>HTML &amp; CSS</li>
-          <li>MySql</li>
-        </ul>
-
-        <a href='http://www.tiftonhoggroup.com' className='btn-red' target='_blank' rel='noopener noreferrer'>View</a>
-      </div>
-    </div>
-
-    <div className='project-item'>
-      <h2>LokaySun</h2>
-
-      <div className='project-copy'>
-        <p>This was a fun project from the <a href='https://freecodecamp.com' target='_blank' rel='noopener noreferrer' style={{color: 'var(--red)', textTransform: 'lowercase'}}>freecodecamp</a> curriculum. This project allows a user to see the temperature and weather condition of their positioning using javascript's geolocation and a weather map API and also lets the user toggle between Farenheit and Celsius. This was my first tango with using an API with javascript so there was a steep learning curve in getting the two to work together. I learned a lot in this process and wrote a tutorial about the process in a blog <a href='https://medium.com/@drewclementsdesign/building-a-weather-app-with-vanilla-javascript-920889a78ca2' target='_blank' rel='noopener noreferrer' style={{color: 'var(--red)', textTransform: 'lowercase'}}>here</a>. I've since moved the article here locally, but the original article continues to receive reads and reactions.</p>
-
-        <h3 style={{
-          marginBottom: 10,
-          fontSize: 18
-        }}>Tech:</h3>
+        <h3
+          style={{
+            marginBottom: 10,
+            fontSize: 18,
+          }}
+        >
+          Tech:
+        </h3>
 
         <ul>
-          <li>Javascript</li>
-          <li>HTML &amp; CSS</li>
-          <li>Weather Map API</li>
+          <li>Node | Express</li>
+          <li>GatsbyJS | React</li>
+          <li>Tailwind CSS</li>
         </ul>
 
-        <a href='https://codepen.io/dckeywork87/pen/paXeKz' className='btn-red' target='_blank' rel='noopener noreferrer'>View</a>
+        <a
+          href="http://fnfapp.com"
+          className="btn-red"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View
+        </a>
 
-        <a href='https://codepen.io/dckeywork87/pen/paXeKz' className='github' target='_blank' rel='noopener noreferrer'>
-          <FontAwesomeIcon className='github' icon={faCodepen}/>
+        <a
+          href="https://github.com/drewclem/fast-and-furious-frontend"
+          className="github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Front-end Repo
+        </a>
+
+        <a
+          href="https://github.com/drewclem/Fast-and-Furious-API"
+          className="github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          API Repo
         </a>
       </div>
     </div>
 
+    <div className="project-item">
+      <h2>Layerframe Animation</h2>
+
+      <div className="project-copy">
+        <p>
+          A fun code assessment style project using NextJS and Framer-motion to
+          build an interesting opening animation. The goal was to create an
+          intro scene that would be a delight to the user while data is being
+          loaded.
+        </p>
+
+        <h3
+          style={{
+            marginBottom: 10,
+            fontSize: 18,
+          }}
+        >
+          Tech:
+        </h3>
+
+        <ul>
+          <li>NextJS | React</li>
+          <li>Tailwind CSS</li>
+          <li>Framer-motion</li>
+        </ul>
+
+        <a
+          href="https://eloquent-benz-e67138.netlify.app/"
+          className="btn-red"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View
+        </a>
+
+        <a
+          href="https://github.com/drewclem/layerframe-animation"
+          className="github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon className="github" icon={faCodepen} />
+        </a>
+      </div>
+    </div>
   </Layout>
 )
 
